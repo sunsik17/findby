223f04692444023d50603d13bb7ae83bb86766ef
sunsik17 <sunsig21c@gmail.com>
2024-01-04 16:29:29 +0900

    product model 생성
    Product 모델 생성
    django 모듈의 db.models.Model을 상속받으면 orm을 이용할 수 있다.
    java의 Entity와 비슷
    -> 데이터 베이스와 매핑되는 객체
    
    views에서 비즈니스 로직을 담당하고
    urls에서 컨트롤러를 매핑한다

--- findby/models.py
+++ findby/models.py
@@ -1,3 +1,16 @@
+from datetime import datetime
+
 from django.db import models
 
+
 # Create your models here.
+
+class Product(models.Model):
+    name: str = models.CharField(max_length=50)
+    category: str = models.CharField(max_length=50)
+    price: str = models.BigIntegerField(default=0)
+    create_date: datetime = models.DateTimeField(auto_now_add=True)
+    modify_date: datetime = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
--- findby/views.py
+++ findby/views.py
@@ -1,3 +1,14 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
+from findby.models import Product
+
+
 # Create your views here.
+def index(request):
+    page = request.GET.get('page', '1')
+    products: [] = Product.objects.order_by('-create_date')
+    paginator: Paginator = Paginator(products, 10)  # page당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    context = {'products': page_obj}
+    return render(request, 'findby/products.html', context)
