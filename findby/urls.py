6d5d3c9b356b0544c9912ae5cd3665228dbcc3ad
sunsik17 <sunsig21c@gmail.com>
2024-01-08 18:56:59 +0900

    [feature-6-search_2] 카테고리 select를 변경하면 해당 category에 해당하는 data만 노출 됌
    - index page에서 category를 변경하면 url=~/category=?으로 ?에 들어오는 값을 이용해 dataset을 바꿈
    - 문제. select를 바꾸는 이벤트에 submit동작을 추가한 거라 자꾸 새로고침 됌
    - 바꾸고 싶은 동작
      - category그대로, category를 default로 바꿀 수 있도록
      - default로 바꾸면 기본 index페이지 반환

--- findby/urls.py
+++ findby/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('search/product', views.search_product, name='search_product'),
     path('delete/products', views.delete_products, name='delete_products'),
-    # path('?category=', views.find_by_category, name='find_by_category'),
 ]
