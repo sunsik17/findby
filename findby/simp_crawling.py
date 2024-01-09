2ebbd63a96bf726e3e38de61210f2884644af70d
sunsik17 <sunsig21c@gmail.com>
2024-01-09 18:34:07 +0900

    [feature-7-add_link] 상품명 클릭 시 상품 페이지이동
    product model field에 link 추가
    template에서 a tag에 target="_blank"로 두고
    {{ product.link }} 해서 추가함
    
    드롭다운 셀렉트 밸류를 그냥 input value와 합쳐서 조회하는 것으로 변경
    드롭다운만 변경해서 조회도 가능
    !주의
    둘다 디폴트값으로 요청시 그냥 index redirect

--- findby/simp_crawling.py
+++ findby/simp_crawling.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup
 from selenium.common import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -12,11 +13,14 @@ def __simp_web_driver() -> webdriver.Chrome:
     return webdriver.Chrome(options=chrome_options)
 
 
-def simp_crawling(words: str) -> str:
+def simp_crawling(words: str, category: str) -> [str]:
     driver = __simp_web_driver()
-    driver.get(KREAM_URL + words)
+    driver.get(KREAM_URL + words + category)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
     try:
-        result = driver.find_element(By.CLASS_NAME, "item_inner").text
+        result = driver.find_element(By.CLASS_NAME, "item_inner").text.split("\n")
+        result.append(category)
+        result.append("https://kream.co.kr" + soup.select_one(".item_inner")["href"])
     except NoSuchElementException:
         result = None
     finally:
--- findby/views.py
+++ findby/views.py
@@ -9,7 +9,6 @@ from findby.model_builder import ProductBuilder
 from findby.models import Product
 from findby.simp_crawling import simp_crawling
 
-
 __DEFAULT_PAGE_NUM = "1"
 
 
@@ -19,30 +18,25 @@ def index(request: HttpRequest) -> HttpResponse:
     category: Union[str, None] = request.GET.get('category', "")
     if category != "":
         products: List[Product] = Product.objects.filter(category=category).order_by('-create_date')
+        redirect('findby:index')
     else:
         products: List[Product] = Product.objects.order_by('-create_date')
 
-    context = __get_context(products, page)
+    context = __get_context(products, page, category)
 
     return render(request, 'findby/products.html', context)
 
 
 def search_product(request: HttpRequest) -> HttpResponse:
     if request.method == 'POST':
-        form = SearchForm(request.POST)
-        if form.is_valid():
-            content = form.cleaned_data['content']
-            data = simp_crawling(words=content)
+        content = str(request.POST['content'])
+        category = str(request.POST['category'])
+        if category != "" or content != "":
+            data = simp_crawling(words=content, category=category)
             if __data_is_valid(data):
                 __to_product(data)
-            return redirect('findby:index')
-    else:
-        form = SearchForm()
 
-    context = {'form': form}
-
-    return render(request, 'findby/products.html', context)
-
+    return redirect('findby:index')
 
 def delete_products(request):
     if request.method == 'POST':
@@ -54,17 +48,19 @@ def delete_products(request):
     return redirect('findby:index')
 
 
-def __to_product(data: str) -> None:
-    product_info = data.split("\n")
+def __to_product(product_info: [str]) -> None:
     brand = product_info[1]
     name = product_info[2]
     amount = product_info[5]
+    category = product_info[len(product_info) - 2]
+    link = product_info[len(product_info) - 1]
 
     product = (ProductBuilder()
                .set_name(name)
                .set_price(amount)
-               .set_category("신발")
+               .set_category(category)
                .set_brand(brand)
+               .set_link(link)
                .build())
 
     product.save()
@@ -76,7 +72,8 @@ def __data_is_valid(data: str) -> bool:
     return True
 
 
-def __get_context(products: List[Product], page: Union[str, None]) -> dict:
+def __get_context(products: List[Product], page: Union[str, None], category: str) -> dict:
     paginator: Paginator = Paginator(products, 10)  # page당 10개씩 보여주기
     page_obj: Paginator = paginator.get_page(page)
-    return {'products': page_obj}
+    return {'products': page_obj,
+            'category': category}
