4b344b857fd10e58485bc9e1225333a3a50d96db
sunsik17 <sunsig21c@gmail.com>
2024-01-11 19:18:13 +0900

    [refactor-1-views] 저장되어 있는 데이터 분류하기
    검색 input에 추가되는 category
    테이블 조회에 적용되는 category를 따로 만들었음,
    
    template은 context dict를 참조함
    Form을 만들어서 사용하는 것이 훨씬 깔끔함
    
    views에서 복잡한 비즈니스로직을 수행하는 것이 아닌
    domain layer에서 복잡한 비즈니스로직을 수행하는 것이 괜찮은 패턴

--- findby/views.py
+++ findby/views.py
@@ -1,10 +1,11 @@
 from typing import Union, List
 
+import django.forms
 from django.core.paginator import Paginator
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 
-from findby.form import SearchForm
+from findby.form import SearchProductForm
 from findby.model_builder import ProductBuilder
 from findby.models import Product
 from findby.simp_crawling import simp_crawling
@@ -14,15 +15,13 @@ __DEFAULT_PAGE_NUM = "1"
 
 # Create your views here.
 def index(request: HttpRequest) -> HttpResponse:
-    page: Union[str, None] = request.GET.get('page', __DEFAULT_PAGE_NUM)
-    category: Union[str, None] = request.GET.get('category', "")
-    if category != "":
-        products: List[Product] = Product.objects.filter(category=category).order_by('-create_date')
-        redirect('findby:index')
-    else:
-        products: List[Product] = Product.objects.order_by('-create_date')
+    page: Union[str, None] = request.GET.get('page')
+    search_category: Union[str, None] = request.GET.get('search_category')
 
-    context = __get_context(products, page, category)
+    products = Product.objects.all().order_by('-create_date')
+    if search_category:
+        products = products.filter(category=search_category)
+    context = __get_context(products, page, search_category)
 
     return render(request, 'findby/products.html', context)
 
@@ -72,8 +71,10 @@ def __data_is_valid(data: str) -> bool:
     return True
 
 
-def __get_context(products: List[Product], page: Union[str, None], category: str) -> dict:
+def __get_context(products: List[Product], page: Union[str, None], search_category: str) -> dict:
     paginator: Paginator = Paginator(products, 10)  # page당 10개씩 보여주기
     page_obj: Paginator = paginator.get_page(page)
-    return {'products': page_obj,
-            'category': category}
+    return {
+        'products': page_obj,
+        'search_category': search_category
+    }
